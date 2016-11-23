f9be5ba162c72967b619c7b565137e749217fdb0
user <user@192.168.1.105>
2016-11-22 19:59:23 -0500

    new commit updated PuzzleGUI

--- puzzlegame/src/puzzlegame/PuzzleGUI.java
+++ puzzlegame/src/puzzlegame/PuzzleGUI.java
@@ -17,7 +17,10 @@ public class PuzzleGUI {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        
         // TODO code application logic here
+        
+        //
     }
     
 }
