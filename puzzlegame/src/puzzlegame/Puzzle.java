67f6dfeedfb64d062dcb648109461faed6110f16
user <user@192.168.1.105>
2016-11-22 19:53:42 -0500

    new commit created PuzzleGUI, Puzzle, AbstractPuzzle, puzzledatatest.json, with copyright notice

---
+++ puzzlegame/src/puzzlegame/Puzzle.java
@@ -0,0 +1,16 @@
+/* Copyright (C) John Celona, Vivian Murga, Brittney Pope, Michael McKenzie, Nick Buehre,
+ * John Mask, and Ryan Hutchinson - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by John Celona, Vivian Murga, Brittney Pope, Michael McKenzie, Nick Buehre,
+ * John Mask, and Ryan Hutchinson, November 2016
+ */
+package puzzlegame;
+
+/**
+ *
+ * @author author
+ */
+public class Puzzle {
+    
+}
---
+++ puzzlegame/src/puzzlegame/PuzzleGUI.java
@@ -0,0 +1,23 @@
+/* Copyright (C) John Celona, Vivian Murga, Brittney Pope, Michael McKenzie, Nick Buehre,
+ * John Mask, and Ryan Hutchinson - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by John Celona, Vivian Murga, Brittney Pope, Michael McKenzie, Nick Buehre,
+ * John Mask, and Ryan Hutchinson, November 2016
+ */
+package puzzlegame;
+
+/**
+ *
+ * @author author
+ */
+public class PuzzleGUI {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
