9ed1774dfdd287a40eeacb4dbdfd8095dd49c566
johnc2016 <jcelona1@student.umuc.edu>
2016-11-23 19:04:00 -0500

    Delete AbstractPuzzle.java

--- puzzlegame/src/puzzlegame/AbstractPuzzle.java
+++
@@ -1,6 +0,0 @@
-/* This file can be removed. The professor said:
- *
- * "I am not really convinced that there is any use for the AbstractPuzzle class in this project. 
- * Just because various authors suggest using abstract classes in some situations does not mean
- * such classes make sense in ALL situations"
- */
